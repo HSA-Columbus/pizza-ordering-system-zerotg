2f2ae3e98d1ad4bca7eb2feda0a513a0d311c161
zerotg <56544563+zerotg@users.noreply.github.com>
2020-02-14 09:18:26 -0500

    Pizza Time

--- index.py
+++ index.py
@@ -3,9 +3,19 @@ from flask import *
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template("options.html")
+
+
+@app.route('/check')
+def check():
+    return render_template("check.html")
+
+
+@app.route('/')
+def main():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
